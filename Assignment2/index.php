3711a4ebb741046c666c403f1f4f227144fd0af5
ekuller <ekuller@wesleyan.edu>
2022-02-25 18:19:06 -0500

    add css and form

--- Assignment2/index.php
+++ Assignment2/index.php
@@ -1,1 +1,29 @@
-<!-- Form -->
+<!DOCTYPE HTML>
+<html lang="en">
+    <body>
+    <link rel="stylesheet" href="css/style.css" />
+
+    <div class="h-center v-center">
+        <h1>music-db</h1>
+        <h2>Registration</h2>
+        <div>
+            <form>
+                <label for="fname">Username:</label>
+                <input  type="text" id="username1" name="u"><br>
+                <label for="lname">Password:</label>
+                <input type="text" id="password" name="p"><br>
+                <input class="button black-text-bg white-text" type="submit" value="Register">
+            </form>
+        </div>
+        <h2>Retrieve Songs By Username</h2>
+        <div>
+            <form>
+                <label for="fname">Username:</label>
+                <input type="text" id="user-ret" name="u-ret"><br>
+                <input class="button black-text-bg white-text"  type="submit" value="Retrieve">
+            </form>
+        </div>
+    </div>
+
+    </body>
+</html>
