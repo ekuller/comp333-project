c5a3c15b1571d8fb386fab2b579d4e3aa853b15e
ekuller <ekuller@wesleyan.edu>
2022-02-25 18:32:37 -0500

    add php code

--- Assignment2/index.php
+++ Assignment2/index.php
@@ -2,24 +2,23 @@
 <html lang="en">
     <body>
     <link rel="stylesheet" href="css/style.css" />
-
     <div class="h-center v-center">
         <h1>music-db</h1>
         <h2>Registration</h2>
         <div>
             <form>
-                <label for="fname">Username:</label>
-                <input  type="text" id="username1" name="u"><br>
-                <label for="lname">Password:</label>
+                <label for="name">Username:</label>
+                <input  type="text" id="username1" name="name"><br>
+                <label for="p">Password:</label>
                 <input type="text" id="password" name="p"><br>
                 <input class="button black-text-bg white-text" type="submit" value="Register">
             </form>
         </div>
         <h2>Retrieve Songs By Username</h2>
         <div>
-            <form>
-                <label for="fname">Username:</label>
-                <input type="text" id="user-ret" name="u-ret"><br>
+            <form method="post" action="retrieve.php">
+                <label for="name">Username:</label>
+                <input type="text" id="user-ret" name="name"><br>
                 <input class="button black-text-bg white-text"  type="submit" value="Retrieve">
             </form>
         </div>
