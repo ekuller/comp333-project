2f4ba390596aa0fda8ad11008069a646ef96382a
Lewis <lewischao9@gmail.com>
2022-03-04 18:44:00 -0500

    add __str__ to models

--- Assignment2/Part2/musicrater/models.py
+++ Assignment2/Part2/musicrater/models.py
@@ -3,13 +3,19 @@ from django.db import models
 class Users(models.Model):
     username = models.CharField(max_length=200, primary_key=True)
     password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.username
 
 class Artists(models.Model):
     song= models.CharField(max_length=200, primary_key=True)
     artist= models.CharField(max_length=200)
+    def __str__(self):
+        return (self.song + " - " + self.artist)
 
 class Ratings(models.Model):
     id = models.AutoField(primary_key=True)
-    username= models.ForeignKey(Users, on_delete=models.CASCADE)
-    song=models.ForeignKey(Artists, on_delete=models.CASCADE)
+    username= models.ForeignKey(Users, on_delete=models.CASCADE, to_field='username')
+    song=models.ForeignKey(Artists, on_delete=models.CASCADE, to_field='song')
     rating=models.IntegerField()
+    def __str__(self):
+        return (self.username.username + " gave " + self.song.song + " a " + str(self.rating))
