0e9acc48e26ddb3b06e119d412551775946c61c6
ekuller <ekuller@wesleyan.edu>
2022-03-04 17:26:25 -0500

    create database schema

--- Assignment2/Part2/musicrater/models.py
+++ Assignment2/Part2/musicrater/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=200, primary_key=True)
+    password = models.CharField(max_length=200)
+
+class Artists(models.Model):
+    song= models.CharField(max_length=200, primary_key=True)
+    artist= models.CharField(max_length=200)
+
+class Ratings(models.Model):
+    id = models.AutoField(primary_key=True)
+    username= models.ForeignKey(Users, on_delete=models.CASCADE)
+    song=models.ForeignKey(Artists, on_delete=models.CASCADE)
+    rating=models.IntegerField()
