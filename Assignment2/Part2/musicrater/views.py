5fe17c68727980eadb657b2e812317e3429cae1e
ekuller <ekuller@wesleyan.edu>
2022-03-04 19:09:47 -0500

    registration functionality

--- Assignment2/Part2/musicrater/views.py
+++ Assignment2/Part2/musicrater/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Users, Artists, Ratings
 
 
 def index(request):
     return render(request, 'musicrater/index.html')
+
+def registerUser(request):
+    if request.method=='POST':
+        if request.POST.get('name') and request.POST.get('pwd'):
+            u=Users()
+            u.username= request.POST.get('name')
+            u.password= request.POST.get('pwd')
+            u.save()
+
+            
