c7deeabe7ad78e5651d8f7fefb76bb49b8f66762
Lewis <lewischao9@gmail.com>
2022-03-06 23:46:59 -0500

    add emoji model

--- Assignment2/Part2/musicrater/models.py
+++ Assignment2/Part2/musicrater/models.py
@@ -21,7 +21,20 @@ class Ratings(models.Model):
     username= models.ForeignKey(Users, on_delete=models.CASCADE, to_field='username')
     song=models.ForeignKey(Artists, on_delete=models.CASCADE, to_field='song')
     rating=models.IntegerField()
+    emoji = models.ForeignKey(EmojiReactions, on_delete=models.SET_NULL, to_field = 'id', null = True)
     class Meta:
         verbose_name_plural = "Ratings"
     def __str__(self):
         return (self.username.username + " gave " + self.song.song + " a " + str(self.rating))
+
+class EmojiReactions(models.Model):
+    id = models.AutoField(primary_key=True)
+    happy = models.BooleanField
+    sad = models.BooleanField
+    celebration = models.BooleanField
+    
+    
+    
+    
+    
+    
