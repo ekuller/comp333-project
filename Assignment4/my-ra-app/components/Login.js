ec697b7f73f00704476f832b905fe574bf9834f6
ekuller <ekuller@wesleyan.edu>
2022-04-26 20:32:32 -0400

    address merge conflicts

--- Assignment4/my-ra-app/components/Login.js
+++ Assignment4/my-ra-app/components/Login.js
@@ -11,16 +11,11 @@ import {
 } from "react-native";
 
 export default function Login(props) {
-<<<<<<< HEAD
 	const verify = (user, pwd) => {
-		if (user === "user" && pwd === "pwd") {
-			props.loginChanger(true);
-		} else {
-			props.loginChanger(false);
-			Alert.alert("The email or password is incorrect.");
-		}
+		props.loginChanger(true);
+		props.userChanger(user);
 	};
-	const [username, setUsername] = useState("");
+
 	const [password, setPassword] = useState("");
 	return (
 		<SafeAreaView style={styles.container}>
@@ -55,7 +50,7 @@ export default function Login(props) {
 					placeholder="Username"
 					autoCorrect={false}
 					autoCapitalize="none"
-					onChangeText={(newText) => setUsername(newText)}
+					onChangeText={(newText) => props.userChanger(newText)}
 				/>
 
 				<Text style={{ fontSize: 20, marginBottom: 5 }}>Password</Text>
@@ -69,71 +64,11 @@ export default function Login(props) {
 				/>
 				<Pressable
 					style={styles.button}
-					onPress={() => verify(username, password)}
+					onPress={() => verify(props.curUsername, password)}
 				>
 					<Text style={styles.text}>Log in</Text>
 				</Pressable>
 			</View>
-=======
-  const verify = (user, pwd) => {
-    props.loginChanger(true);
-    props.userChanger(user);
-  };
-
-  const [password, setPassword] = useState("");
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "white",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 35,
-          }}
-        >
-          Music Rater Login
-        </Text>
-      </View>
-
-      <View
-        style={{
-          flex: 1.5,
-          backgroundColor: "white",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <Text style={{ fontSize: 20, marginBottom: 5 }}>Username</Text>
-        <TextInput
-          style={styles.inputContainer}
-          placeholder="Username"
-          autoCorrect={false}
-          autoCapitalize="none"
-          onChangeText={(newText) => props.userChanger(newText)}
-        />
-
-        <Text style={{ fontSize: 20, marginBottom: 5 }}>Password</Text>
-        <TextInput
-          style={styles.inputContainer}
-          placeholder="Password"
-          secureTextEntry={true}
-          autoCorrect={false}
-          autoCapitalize="none"
-          onChangeText={(newText) => setPassword(newText)}
-        />
-        <Pressable
-          style={styles.button}
-          onPress={() => verify(props.curUsername, password)}
-        >
-          <Text style={styles.text}>Log in</Text>
-        </Pressable>
-      </View>
->>>>>>> cb54be95cb9c5269e830a14b24907578903c218d
 
 			<StatusBar style="auto" />
 		</SafeAreaView>
